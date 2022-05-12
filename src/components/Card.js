740868b21c6cab23d0e295518dd890dcc2664f86
Isabel Terenzi <bel.terenzi@gmail.com>
2022-05-12 20:51:40 -0300

    arruma lint

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ class Card extends React.Component {
     return (
       <div className="container-card">
         <h1 data-testid="name-card">{cardName}</h1>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card"/>
+        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
         <p data-testid="description-card">{cardDescription}</p>
         <h2 data-testid="attr1-card">{cardAttr1}</h2>
         <h2 data-testid="attr2-card">{cardAttr2}</h2>
